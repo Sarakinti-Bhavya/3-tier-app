6a57ae49bf6a4825f9058e2494ab4b4c92344570
Sarakinti-Bhavya <83421134+Sarakinti-Bhavya@users.noreply.github.com>
2026-04-27 14:04:09 +0530

    Create EmployeeController.java

---
+++ src/main/java/com/company/demo/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.company.demo.controller;
+
+import com.company.demo.model.Employee;
+import com.company.demo.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is UP";
+    }
+}
